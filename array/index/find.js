b6974856863a5926d28dc61328cd7689724d4070
Tejavardhan-Naidu <teju44915@gmail.com>
2021-12-14 18:35:36 +0530

    okie

---
+++ array/index/find.js
@@ -0,0 +1,10 @@
+let size=[10,20,30,40];
+let size1=[
+    { name:"teja",salary:50000},
+      {name:"mano",salary:100000}
+    ];
+console.log(size);
+console.log(size.indexOf(20));
+console.log(size.lastIndexOf(size));
+console.log(size1[0].name);
+console.log(size1[1].salary);
