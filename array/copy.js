621d986206824b78b167ac8f1979de6e1f602aa2
Tejavardhan-Naidu <teju44915@gmail.com>
2021-12-14 16:22:23 +0530

    okie

---
+++ array/copy.js
@@ -0,0 +1,6 @@
+let size=[10,20,30,40];
+let new_size=[...size];
+console.log(size);
+console.log(new_size);
+console.log(size.indexOf(20));
+console.log(size.indexOf(40));
---
+++ array/slicing.js
@@ -0,0 +1,7 @@
+let size=[101,102,103,104];
+console.log(size.slice(1,3));
+console.log(size);
+console.log(size.slice(0,2));
+console.log(size);
+console.log(size.splice(0,3));
+console.log(size);
